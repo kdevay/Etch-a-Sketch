8d608609afd5753aac1b1c811f7489e171780af5
Katherine de Vay <katdevay2@gmail.com>
2022-09-09 12:07:41 -0500

    Satisfy MVP, begin popup construction

--- index.js
+++ index.js
@@ -1,62 +1,46 @@
+// Changes number of grid divs
+function resizeGrid(event) {
+    //event.target.setAttribute('class', 'filled'); 
+    divGrid = [];
+    for(var i = 0; i < INPUT; i++) {
+        divGrid[i]=document.createElement("div");// create new div
+        content.appendChild(divGrid[i]);// add to dom
+        divGrid[i].addEventListener('mouseover', changeColor);// Add color change on hover
+        
+    }
+}
+
+// Creates popup window
+function
+
+// Changes color of grid divs
+function changeColor(event) {
+    event.target.setAttribute('class', 'filled'); 
+}
+
+// Import content
+const heading = document.getElementById('heading');
+const content = document.getElementById("content");
+
 // Create grid
-// Add event listener to grid divs on hover
+var divGrid = [];
+for(var i = 0; i < 256; i++) {
+    divGrid[i] = document.createElement("div");// create new div
+    content.appendChild(divGrid[i]);// add to dom
+    divGrid[i].addEventListener('mouseover', changeColor);// Add color change on hover    
+}
+
 // Create button
-// Add event listener to button that generates popup on click
+const resizeButton = document.createElement('button');
+resizeButton.textContent = 'new sketchpad';// add inner text
+heading.appendChild(resizeButton);// add to dom
+resizeButton.addEventListener('click', openPopup);// open popup on click
+console.log(resizeButton);
+
 // Add event listener to popup
 
 /*
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body{
-  background-color: #2e2e2d;
-  color: white;
-}
-.slidecontainer {
-  width: 100%;
-}
-
-.slider {
-  -webkit-appearance: none;
-  width: 100%;
-  height: 15px;
-  border-radius: 5px;
-  background: #f5c7b8;
-  outline: none;
-  opacity: 0.7;
-  -webkit-transition: .2s;
-  transition: opacity .2s;
-}
-
-.slider:hover {
-  opacity: 1;
-}
-
-.slider::-webkit-slider-thumb {
-  -webkit-appearance: none;
-  appearance: none;
-  width: 25px;
-  height: 25px;
-  border-radius: 50%;
-  background: #79d9bf;
-  cursor: pointer;
-}
-
-.slider::-moz-range-thumb {
-  width: 25px;
-  height: 25px;
-  border-radius: 50%;
-  background: #04AA6D;
-  cursor: pointer;
-}
-</style>
-</head>
-<body>
-
-<h1>Custom Range Slider</h1>
-<p>Drag the slider to display the current value.</p>
+Slide sizer
 
 <div class="slidecontainer">
   <input type="range" min="16" max="100" value="16" class="slider" id="myRange">
